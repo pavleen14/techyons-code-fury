ecb5c4a2775508ecea54b2b80a29dcf200d45db4
pavleen14 <pavonweb99@gmail.com>
2021-09-19 18:24:18 +0530

    basic meeting room structure

---
+++ meetingroommanagement/src/main/java/com/hsbc/meets/exception/MeetingRoomAlreadyExistsException.java
@@ -0,0 +1,20 @@
+/**
+ * The MeetingRoomAlreadyExistsException class
+ * 
+ * @author pavleen
+ * @since 0.0.1
+ */
+
+package com.hsbc.meets.exception;
+
+public class MeetingRoomAlreadyExistsException extends Exception {
+
+	public MeetingRoomAlreadyExistsException() {
+		super();
+	}
+
+	public MeetingRoomAlreadyExistsException(String message) {
+		super(message);
+	}
+
+}
