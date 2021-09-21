3f932882dca3e8f38656c9b67904783256eb6364
pavleen14 <pavonweb99@gmail.com>
2021-09-21 10:39:50 +0530

    deleting package-info files

--- meetingroommanagement/src/main/java/com/hsbc/meets/controller/package-info.java
+++
@@ -1,9 +0,0 @@
-/**
- * The controller subpackage will hold the application
- * specific HttpServelts. This subpackage forms our 
- * controller layer
- * 
- * @author alan
- * @since 0.0.1
- */
-package com.hsbc.meets.controller;
