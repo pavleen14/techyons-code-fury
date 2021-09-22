7af1d818e14afbdb422fc415e539391d9b2546e9
Shubhra Bhunia Ghosh <shubhramamon@gmail.com>
2021-09-22 22:47:10 +0530

    by mistake added sql files last time

--- sql/feedback_ins_trigger.sql
+++
@@ -1,9 +0,0 @@
-delimiter //
-create trigger feedback_ins 
-after insert 
-on feedback
-for each row
-update meetingroom set rating=(select avg(Rating) from Feedback where MeetingRoomId= NEW.MeetingRoomId), 
-NumberOfFeedbacks= coalesce(NumberOfFeedbacks,0)+1 
-where MeetingRoomId= NEW.MeetingRoomId;//
-delimiter ;
