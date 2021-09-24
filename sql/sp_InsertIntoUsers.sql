bf8ffdc51a3e119ca881136d4a73e0b13cb34637
Rashmikareddya <rashmikareddya@gmail.com>
2021-09-24 09:55:00 +0530

    Updated InsertIntoUsers

--- sql/sp_InsertIntoUsers.sql
+++ sql/sp_InsertIntoUsers.sql
@@ -3,9 +3,9 @@ DROP procedure IF EXISTS `sp_InsertIntoUsers`;
 
 DELIMITER $$
 USE `meeting_room_booking_db`$$
-CREATE PROCEDURE `sp_InsertIntoUsers` ( IN NameInput varchar(20),IN EmailInput varchar(30),IN PhoneInput varchar(30),IN CreditsInput INT, IN RoleInput varchar(10),IN PasswordInput varchar(30))
+CREATE PROCEDURE `sp_InsertIntoUsers` ( IN NameInput varchar(45),IN EmailInput varchar(45),IN PhoneInput varchar(13),IN CreditsInput INT, IN RoleInput varchar(10),IN PasswordInput varchar(32))
 BEGIN
-		INSERT INTO tbl_users (ID, Name, Email, Phone, Credits, Role, Password) VALUES(ceiling(RAND()*1000000),NameInput,EmailInput,PhoneInput,CreditsInput,RoleInput,PasswordInput);
+		INSERT INTO tbl_users (Name, Email, Phone, Credits, Role, Password) VALUES(NameInput,EmailInput,PhoneInput,CreditsInput,RoleInput,PasswordInput);
 END$$
 
 DELIMITER ;
