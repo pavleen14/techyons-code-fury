d8279d74ddfa945ec61e44cf690f3a1dfa939b49
Alan P John <alanpjohn@outlook.com>
2021-09-25 12:26:07 +0530

    optimised amenity insertion
    Now we dont need the amenity ID when we insert amenities

--- sql/sp_InsertAmenityInMeetingRoomAmenities.sql
+++ sql/sp_InsertAmenityInMeetingRoomAmenities.sql
@@ -3,9 +3,12 @@ DROP procedure IF EXISTS `sp_InsertAmenityInMeetingRoomAmenities`;
 
 DELIMITER $$
 USE `meeting_room_booking_db`$$
-CREATE PROCEDURE `sp_InsertAmenityInMeetingRoomAmenities` (IN AmenityIdInput INT, IN MeetingRoomIdInput INT)
+CREATE PROCEDURE `sp_InsertAmenityInMeetingRoomAmenities` (IN AmenityNameInput VARCHAR(30), IN MeetingRoomIdInput INT)
 BEGIN
-INSERT INTO tbl_meeting_room_amenities (AmenityId,MeetingRoomId) VALUES (AmenityIdInput, MeetingRoomIdInput );
+INSERT INTO tbl_meeting_room_amenities (AmenityId,MeetingRoomId) VALUES ( 
+  (SELECT AmenityId FROM tbl_amenities WHERE AmenityName=AmenityNameInput) ,
+  MeetingRoomIdInput 
+);
 END$$
 
 DELIMITER ;
