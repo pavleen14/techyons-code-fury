e4396d0f27a69b21f31fd88d1940747460ddd5d9
Alan P John <alanpjohn@outlook.com>
2021-09-26 15:20:03 +0530

    Adjustment
    Sorry, forgot to add this earlier

--- sql/sp_ShowAllMeetingRooms.sql
+++ sql/sp_ShowAllMeetingRooms.sql
@@ -2,11 +2,20 @@ USE `meeting_room_booking_db`;
 DROP PROCEDURE IF EXISTS `sp_ShowAllMeetingRooms`;
 
 DELIMITER $$
-USE `meeting_room_booking_db`$$
 CREATE PROCEDURE `sp_ShowAllMeetingRooms` ()
 BEGIN
-			SELECT*  FROM tbl_meetingroom;
-END$$
+	SELECT 
+    mr.MeetingRoomId,
+    mr.Name,
+    mr.SeatingCapacity,
+    mr.Rating,
+    mr.PerHourCost,
+    a.AmenityName
+    FROM tbl_meetingroom mr
+    JOIN tbl_meeting_room_amenities mra
+    JOIN tbl_amenities a
+    ON mra.MeetingRoomId = mr.MeetingRoomId
+    AND a.AmenityId = mra.AmenityId;
+END $$
 
 DELIMITER ;
-
