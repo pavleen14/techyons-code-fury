32a7b47e75c808fccbad850c5813398f486b004f
Alan P John <alanpjohn@outlook.com>
2021-09-26 13:50:58 +0530

    fix syntax

--- sql/tr_UpdateNumberOfMeetings.sql
+++ sql/tr_UpdateNumberOfMeetings.sql
@@ -1,10 +1,12 @@
-DROP TRIGGER IF EXISTS 
-delimiter //
+DROP TRIGGER tr_UpdateNumberOfMeetings IF EXISTS; 
+delimiter $$
 create trigger tr_UpdateNumberOfMeetings
 after insert
 on tbl_meeting
 for each row
+
 update tbl_meetingroom set
 NumberOfMeetings= coalesce(NumberOfMeetings,0)+1 
-where MeetingRoomId= NEW.MeetingRoomId;//
+where MeetingRoomId= NEW.MeetingRoomId;
+$$
 delimiter ;
