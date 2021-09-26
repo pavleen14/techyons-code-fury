f65e23ca0706048110bf48431b6ab0457ecfa498
Alan P John <alanpjohn@outlook.com>
2021-09-26 18:14:50 +0530

    updated the upcoming meeting
    @muskansingh11 Check this out

--- sql/sp_GetUpcomingMeetings.sql
+++ sql/sp_GetUpcomingMeetings.sql
@@ -6,13 +6,14 @@ USE `meeting_room_booking_db`$$
 CREATE PROCEDURE `sp_GetUpcomingMeetings` (IN EmailInput varchar(45))
 BEGIN
 select ID into @userid from tbl_users where Email = EmailInput;
-select m.*
+select m.Title, m.TypeOfMeeting , mr.Name , mr.MeetingRoomId , m.OrganizedBy , m.StartTme
 from tbl_meeting m
 join tbl_attendee a
+join tbl_meetingroom mr
 on m.meetingId=a.meetingId
+AND mr.MeetingRoomId = m.MeetingRoomId
 where a.userId=@userid
-and m.StartTme >=current_timestamp();
+and m.StartTme >= NOW();
 END$$
 
 DELIMITER ;
-
